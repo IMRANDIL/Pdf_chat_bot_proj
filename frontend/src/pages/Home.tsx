81ca523fd53dffdc5ced37bc4b0700da76a1929d
IMRANDIL <aliimranadil2@gmail.com>
2024-09-14 20:54:54 +0530

    some more ui

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,8 +6,8 @@ const Home: React.FC = () => {
   return (
     <div className="home-container">
       <div className="home-content">
-        <h1 className="home-title">Document Q&A</h1>
-        <p className="home-description">
+        <h1 className="home-title" style={{textAlign:'center'}}>Document Q&A</h1>
+        <p className="home-description" style={{textAlign:'center'}}>
           Ask any question about the documents and get accurate responses based on their content.
         </p>
         <DocumentQAForm />
