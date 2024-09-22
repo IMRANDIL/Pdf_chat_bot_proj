1cc750d5c7ab1d4ec71dd51cabaf47e8ee03fcce
IMRANDIL <aliimranadil2@gmail.com>
2024-09-22 09:03:57 +0530

    more ui fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,28 @@ import Home from './pages/Home';
 import DocumentQAForm from './components/DocumentQA/DocumentQAForm';
 import './App.css';
 import gifyBot from './assets/giphy_bot.gif';
+import { useUser } from '@clerk/clerk-react'
+import backgroundImage from './assets/black-white-background.jpg'
 
 
 
 const App: React.FC = () => {
+  const {user} = useUser()
+
+  // Access the email address
+const email = user && user.emailAddresses[0].emailAddress;
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <>
+    {!email && <h2 style={{textAlign:'center'}}>Please Sign In to chat your doc(pdf)</h2>}
+    <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', backgroundImage: `url(${!email && backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
       <header style={{ textAlign: 'center' }}>
+       
         <SignedOut>
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <img 
               src={gifyBot}
               alt="PDF Chat Bot" 
-              style={{ width: '100%', maxWidth: '200px', marginBottom: '20px', borderRadius: '8px' }} 
+              style={{ width: '100%', maxWidth: '200px', marginBottom: '20px', borderRadius: '8px', marginTop:'18px' }} 
             />
             <SignInButton>
             <button style={{ backgroundColor: '#007bff', color: 'white', padding: '10px 16px', border: 'none', borderRadius: '4px', cursor: 'pointer', fontSize: '16px' }}>Sign in</button>
@@ -33,6 +42,7 @@ const App: React.FC = () => {
         </SignedIn>
       </header>
     </div>
+    </>
   );
 };
 
